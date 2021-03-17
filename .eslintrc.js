51a34e0e245371fa7b4a0775c1fc5b7f4df84bfa
Zwel Pai <lozberipie@gmail.com>
2021-03-17 15:49:33 +0700

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+  },
+};
---
+++ src/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <setup></setup>
+</template>
+
+<script>
+// IGNORE THIS FILE FOR NOW
+import Setup from "./src/setup.vue";
+export default {
+  components: { Setup }
+};
+</script>
