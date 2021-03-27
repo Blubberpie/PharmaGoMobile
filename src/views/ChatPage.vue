d82fa5d52373eec2768343caa06583cff33b8d6a
pao741 <anuvt741@gmail.com>
2021-03-27 15:03:58 +0700

    add modules/implement router

---
+++ src/views/ChatPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">Chaaa Pageee</text>
+    <button title="Press me!" @press="exclaim" />
+  </view>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      message: '',
+    };
+  },
+  mounted() {
+    this.message = 'Hello World'; // testing mounted
+  },
+  methods: {
+    exclaim() {
+      this.message += '!';
+      // eslint-disable-next-line no-alert
+      alert(this.message);
+    },
+  },
+};
+</script>
+
+<style>
+.container {
+  background-color: white;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+}
+.text-color-primary {
+  color: blue;
+}
+</style>
